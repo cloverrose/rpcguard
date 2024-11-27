6115ffe55ebcd1dea71aad2b0a07de9245b8a6c6
Kenji Osari <kosari@mercari.com>
2024-11-27 09:39:25 +0900

    feat: support golangci-lint plugin

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/cloverrose/rpcguard
 
 go 1.23
 
-require golang.org/x/tools v0.27.0
+require (
+	github.com/golangci/plugin-module-register v0.1.1
+	golang.org/x/tools v0.27.0
+)
---
+++ golangci_plugin.go
@@ -0,0 +1,48 @@
+package rpcguard
+
+import (
+	"golang.org/x/tools/go/analysis"
+
+	"github.com/golangci/plugin-module-register/register"
+)
+
+func init() {
+	RegisterPlugin()
+}
+
+func RegisterPlugin() {
+	// https://golangci-lint.run/plugins/module-plugins/
+	register.Plugin("rpcguard", newPlugin)
+}
+
+func newPlugin(conf any) (register.LinterPlugin, error) {
+	s, err := register.DecodeSettings[settings](conf)
+	if err != nil {
+		return nil, err
+	}
+
+	return &plugin{settings: &s}, nil
+}
+
+type settings struct {
+	Verbose bool
+}
+
+type plugin struct {
+	settings *settings
+}
+
+func (p *plugin) BuildAnalyzers() ([]*analysis.Analyzer, error) {
+	if p.settings.Verbose {
+		Verbose = p.settings.Verbose
+	}
+	return []*analysis.Analyzer{
+		Analyzer,
+	}, nil
+}
+
+func (p *plugin) GetLoadMode() string {
+	return register.LoadModeSyntax
+}
+
+var _ register.LinterPlugin = &plugin{}
